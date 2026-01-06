03ee13c480114023a890c9828699be920b424c48
aarav <aravlead@gmail.com>
2026-01-06 12:07:08 +0530

    Json logging

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-const path=require('path');
-const bodyParser=require('body-parser');
-const express=require('express');
-const app=express();
-app.set('view engine','ejs');
-app.set('views','views');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
-const fakeposts=[
-{
-"id":1,
-"description":"post1",
-"content":"Hi this is the first post,it is about a travel blog ",
-},
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const express = require('express');
+const { readFile } = require('fs');
+const { error } = require('console');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-{
-"id":2,
-"description":"post2",
-"content":"Hi this is the second post this is about food and cuisines all over the world",
-},
-];
-
-function getpostsbyid(id){
-   for(let i=0;i<fakeposts.length;i++){
-         if(fakeposts[i].id===id){
-            return fakeposts[i];
-         }
-
-   } 
-
+function getpostfromfile(){
+   try {
+      const realdata=fs.readFileSync('./data/posts.json');
+      const arra=JSON.parse(realdata);
+      return arra.posts||[];
+   }
+    catch (error) {
+      console.log("error"+error.message);
+      return [];
+   }
+}
+const arr=getpostfromfile();
+function getpostsbyid(id) {
+   for (let i = 0; i <arr.length; i++) {
+      if (arr[i].id === id) {
+         return arr[i];
+      }
+   }
    return null;
 }
+app.get('/HomePage', (req, res, next) => {
+   res.render("index", {
+      pageTitle: "Homepage",
+      posts:arr
 
-
-app.get('/HomePage',(req,res,next)=>{
-res.render("index",{
-   pageTitle:"Homepage",
-   posts:fakeposts
-
-
-});
+   });
 });
 
-app.get('/Posts/:id',(req,res,next)=>{
-const Postid=req.params.id;
-const Post=getpostsbyid(parseInt(Postid));
-if(!Post){
-   return  res.status(404).send('Page not found');
-}
-res.render("post",
-   {pageTitle:"Post.description",
-      post:Post,
-      link:"/Homepage"
-   })
+app.get('/Posts/:id', (req, res, next) => {
+   const Postid = req.params.id;
+   const Post = getpostsbyid(parseInt(Postid));
+   if (!Post) {
+      return res.status(404).send('Page not found');
+   }
+   res.render("post",
+      {
+         pageTitle: "Post.description",
+         post: Post,
+         link: "/Homepage"
+      })
 });
 console.log('http://localhost:3000')
 app.listen(3000);
