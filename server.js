eaab288b46b3d6e42937212a5ad689108cf44594
aarav <aravlead@gmail.com>
2026-01-06 10:44:28 +0530

    Separate Post Routes for each Post Page

--- server.js
+++ server.js
@@ -7,24 +7,51 @@ app.set('views','views');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 const fakeposts=[
-
 {
+"id":1,
 "description":"post1",
 "content":"Hi this is the first post,it is about a travel blog ",
 },
 
 {
+"id":2,
 "description":"post2",
 "content":"Hi this is the second post this is about food and cuisines all over the world",
 },
 ];
 
+function getpostsbyid(id){
+   for(let i=0;i<fakeposts.length;i++){
+         if(fakeposts[i].id===id){
+            return fakeposts[i];
+         }
+
+   } 
+
+   return null;
+}
+
+
 app.get('/HomePage',(req,res,next)=>{
-res.render("index",{pageTitle:"Homepage",posts:fakeposts});
+res.render("index",{
+   pageTitle:"Homepage",
+   posts:fakeposts
+
+
 });
-app.use('/',(req,res,next)=>{
-res.status(404).send('Page not found');
 });
 
+app.get('/Posts/:id',(req,res,next)=>{
+const Postid=req.params.id;
+const Post=getpostsbyid(parseInt(Postid));
+if(!Post){
+   return  res.status(404).send('Page not found');
+}
+res.render("post",
+   {pageTitle:"Post.description",
+      post:Post,
+      link:"/Homepage"
+   })
+});
 console.log('http://localhost:3000')
 app.listen(3000);
