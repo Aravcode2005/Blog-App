84c7d2e24bcc26cd6b4aae77da76a62a07139335
aarav <aravlead@gmail.com>
2026-01-11 10:20:58 +0530

    Write panel

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-let isLoggedin=false;
+let isLoggedin = false;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 function getpostfromfile() {
@@ -19,9 +19,12 @@ function getpostfromfile() {
       return [];
    }
 }
-
 const arr = getpostfromfile();
-const postcount=arr.length;
+function createpost(){
+const newid=arr.length+1;
+arr.push(newid);
+}
+const postcount = arr.length;
 function getpostsbyid(id) {
    for (let i = 0; i < arr.length; i++) {
       if (arr[i].id === id) {
@@ -30,11 +33,15 @@ function getpostsbyid(id) {
    }
    return null;
 }
+app.get('/', (req, res, next) => {
+   res.status(404).send("Page not found");
+   next();
+});
+
 app.get('/HomePage', (req, res, next) => {
    res.render("index", {
       pageTitle: "HomePage",
       posts: arr
-
    });
 });
 app.get('/Posts/:id', (req, res, next) => {
@@ -57,29 +64,40 @@ app.get('/login', (req, res, next) => {
    });
 });
 app.get('/admin', (req, res, next) => {
-   if(!isLoggedin){
-     res.redirect('/HomePage');
-        return;
+   if (!isLoggedin) {
+      res.redirect('/HomePage');
+      return;
    }
    res.render("dynamicadmin", {
       pageTitle: "Adminpanel",
-      POST:postcount,
+      POST: postcount,
    });
 });
 app.post('/login', (req, res, next) => {
    const { password } = req.body;
    console.log({ password });
    try {
-       if (password === 'abcd123') {
-         isLoggedin=true;
-        res.redirect('/admin');
-   }
+      if (password === 'abcd123') {
+         isLoggedin = true;
+         res.redirect('/admin');
+      }
    } catch (error) {
-      console.log("Laadle" + error.message);
+      console.log(error.message);
    }
 });
-app.get('/',(req,res,next)=>{
-res.status(404).send("Page not found");
+
+app.get('/write',(req,res,next)=>{
+
+res.render("newpost",
+      {
+         pageTitle: "Userpage"
+      });
 });
+
+app.post('/write', (req, res, next) => {
+   const {inputtext} =req.body;
+   console.log({ inputtext });
+});
+
 console.log('http://localhost:3000')
 app.listen(3000);
